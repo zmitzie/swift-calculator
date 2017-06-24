4ef7c894f0d79ecc193daa25a9e8c28bb47f6f9b
Zacharias Mitzelos <accounts@mitzelos.com>
2017-06-24 16:49:38 +0300

    Create basic calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypping = true
+            
+        }
+        }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTypping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+                case "π":
+                    displayValue = Double.pi
+                case "√":
+                    displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
     }
-
-
 }
 
